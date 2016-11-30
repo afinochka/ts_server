be66f19cfb667ff95dc8275ab9c638ec124ee609
kstdl <kostello.d@gmail.com>
2016-12-01 00:35:21 +0300

    download\upload of images added

--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
+/// <reference path="typings/async-busboy.d.ts" />
+
 import * as koa from 'koa';
 import * as koaRouter from 'koa-router';
 import * as koaBodyparser from 'koa-bodyparser';
 import * as fs from 'fs';
+import * as crypto from 'crypto';
 import * as bodyParser from './utils'
 import serverFn from './server';
 import thunk from './controllers/thunk';
 import mysql = require('./controllers/mysql');
+import asyncBusboy = require('async-busboy');
+
+async function read_file(path: string) {
+    return await thunk(fs.readFile, [path], fs);
+}
 
 (async () => {
     var app = new koa();
@@ -13,7 +21,20 @@ import mysql = require('./controllers/mysql');
     app.use(koaBodyparser());
     router.get('/', async (ctx) => {
         ctx.body = await thunk(fs.readFile, ['./views/index.html', 'utf-8'], fs);
+    });
 
+    router.post('/upload', async function(ctx, next) {
+        const {files, fields} = await asyncBusboy(ctx.req);
+        let start = Math.floor(Math.random() * 54);
+        let hash = crypto.createHash('sha256').update(JSON.stringify(files[0])).digest('hex').substring(start, start + 10)
+        let fileType = '.' + files[0].mimeType.substring(files[0].mimeType.search('/') + 1);
+        files[0].pipe(fs.createWriteStream(hash + fileType));
+        ctx.body = "OK"
+    });
+
+    router.get('/pepe', async (ctx) => {
+        ctx.type = 'image/jpeg';
+        ctx.body = await read_file('./views/pepe.jpg');
     });
 
     router.get('/:table', async (ctx) => {
